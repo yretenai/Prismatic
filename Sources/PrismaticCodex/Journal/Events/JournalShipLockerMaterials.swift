c031dd56568838439b9759a61a7c2e37c7011682
Ada <yretenai@gmail.com>
2025-01-21 14:34:45 +0000

    event: codegen empty classes for every event

---
+++ Sources/PrismaticCodex/Journal/Events/JournalShipLockerMaterials.swift
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: 2025 Legiayayana <ada@chronovore.dev>
+// SPDX-License-Identifier: EUPL-1.2
+
+import Foundation
+
+/// TODO: Fill me out
+public class JournalShipLockerMaterials: JournalEntry {
+	required init(json: [String: Any], event: JournalEvent) {
+		// todo
+
+		super.init(json: json, event: event)
+	}
+
+	public override var description: String {
+		"\(super.description)"
+	}
+}
