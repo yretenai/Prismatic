807d150f894825248080d7d7e64a67c1746378f3
Ada <yretenai@gmail.com>
2025-02-10 14:46:40 +0000

    chore: fix windows

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
 	],
 	dependencies: [
 		.package(url: "https://github.com/yretenai/FileMonitor.git", branch: "main"),
-		.package(url: "https://github.com/rensbreur/SwiftTUI.git", revision: "537133031bc2b2731048d00748c69700e1b48185"),
+		.package(url: "https://github.com/yretenai/SwiftTUI.git", branch: "main"),
 	],
 	targets: [
 		.target(
